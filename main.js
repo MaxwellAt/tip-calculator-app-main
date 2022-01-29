b899a56fac6b045faab6972ee2583d3fba4d7bfa
Jêfferson Maxwell <jeffersonmaxwell1603@gmail.com>
2022-01-28 21:08:42 -0300

    update dia 28

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-const formulario = $('.form-control');
+function e(){
+    const valor = parseInt(document.getElementById('input2').value);
+    const valor2 = document.getElementById('input2');
+    const alerta = document.getElementById('alerta');
+    if (valor === 0 ){
+        alerta.style.display = 'block';
+        valor2.style.boxShadow = '0 0 0 0.25rem brown';
+    }
+    else {
+        alerta.style.display = 'none';
+        valor2.style.boxShadow = 'none';
+    }
+}
+function b(){
+    const input1 = parseInt(document.getElementById('entrada1').value);
+    if ( input1 > 0 || input1 < 0){
+        document.getElementById('reset').removeAttribute('disabled');
+    }
+    else{
+        document.getElementById('reset').setAttribute('disabled' , 'disabled');
+    }
+}
